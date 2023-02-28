17a26c7a0e8b278063ca1b386a1b0b346a3fc9a5
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-28 14:19:16 -0300

    express y rutas

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Hola desde Node')
+const express = require("express");
+
+// Crear el servidor/aplicacion de express
+
+const app = express();
+
+// GET
+app.get("/", (req, res) => {
+	console.log("peticion de /");
+	res.status(200).json({
+		ok: true,
+		msg: "Todo salio bien",
+		uid: 1234,
+	});
+});
+
+app.listen(4000, () => {
+	console.log(`Servidor corriendo en puerto ${4000}`);
+});
