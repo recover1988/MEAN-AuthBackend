7a8581015e0771ca24fdad16b4e24220b2038e7d
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-28 20:58:23 -0300

    crear usuario en DB

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,36 @@
 const { response, request } = require("express");
+const Usuario = require("../models/Usuario");
 
-const crearUsuario = (req = request, res = response) => {
+const crearUsuario = async (req = request, res = response) => {
 	const { name, email, password } = req.body;
+	try {
+		// Verificar el email en DB
+		const usuario = await Usuario.findOne({ email });
 
-	return res.json({
-		ok: true,
-		msg: "Crear usuario /new",
-	});
+		if (usuario) {
+			return res.status(400).json({
+				ok: false,
+				msg: "El usuario ya existe con ese email",
+			});
+		}
+		// Crear usuario con el modelo
+		const dbUser = new Usuario(req.body);
+		// Hashear la contraseña
+		// Generar el JWT -> metodo de autentificacion pasiva
+		// Crear usuario de DB
+		await dbUser.save();
+		// Generar respuesta exitosa
+		return res.status(201).json({
+			ok: true,
+			uid: dbUser.id,
+			name,
+		});
+	} catch (error) {
+		return res.status(500).json({
+			ok: false,
+			msg: "Por favor hable con el administrador",
+		});
+	}
 };
 
 const loginUsuario = (req = request, res = response) => {
