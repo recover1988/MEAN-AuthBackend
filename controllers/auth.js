ddfc96245d5cb4c3ae95b7ad9ef716397716c042
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-28 17:08:14 -0300

    validar campos obligatorios

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { response, request } = require("express");
+const { validationResult } = require("express-validator");
 
 const crearUsuario = (req = request, res = response) => {
 	const { name, email, password } = req.body;
@@ -9,7 +10,18 @@ const crearUsuario = (req = request, res = response) => {
 	});
 };
 
-const loginUsuario = (req, res) => {
+const loginUsuario = (req = request, res = response) => {
+	// Verificar si tiene errores
+	const errors = validationResult(req);
+
+	if (!errors.isEmpty()) {
+		return res.json({
+			ok: false,
+			errors: errors.mapped(),
+		});
+	}
+
+	// Sino tiene errores responde
 	const { email, password } = req.body;
 	console.log(email, password);
 	return res.json({
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const { Router } = require("express");
+const { check } = require("express-validator");
 const {
 	crearUsuario,
 	revalidaToken,
@@ -10,7 +11,14 @@ const router = Router();
 // Crear un nuevo usuario // Controlador de la ruta
 router.post("/new", crearUsuario);
 // Login de Usuario
-router.post("/", loginUsuario);
+router.post(
+	"/",
+	[
+		check("email", "El email es obligatorio").isEmail(),
+		check("password", "La contraseña es obligatorio").isLength({ min: 6 }),
+	],
+	loginUsuario
+);
 // Validar y revalidar token
 router.get("/renew", revalidaToken);
 
