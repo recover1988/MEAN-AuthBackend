f79284cf6c3f4837e58c4517bd1b29dd5a48ae01
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-02-28 17:36:55 -0300

    info

--- routes/auth.js
+++ routes/auth.js
@@ -9,7 +9,16 @@ const {
 const router = Router();
 
 // Crear un nuevo usuario // Controlador de la ruta
-router.post("/new", crearUsuario);
+router.post(
+	"/new",
+	[
+		check("name", "El nombre es obligatorio").notEmpty(),
+		check("name", "El nombre minimo de 6").isLength({ min: 6 }),
+		check("email", "El email es obligatorio").isEmail(),
+		check("password", "La contraseña es obligatoria").isLength({ min: 6 }),
+	],
+	crearUsuario
+);
 // Login de Usuario
 router.post(
 	"/",
