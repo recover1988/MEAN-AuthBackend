0efe626b4a3282f0ce213d36806383a2655872c1
Eric Denis Laura <108999458+recover1988@users.noreply.github.com>
2023-03-02 15:59:31 -0300

    configuracion en backend

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const { dbConnection } = require("./db/config");
+
 require("dotenv").config();
 
 // Variables de Entorno
@@ -24,6 +26,11 @@ app.use(express.json());
 
 // Rutas
 app.use("/api/auth", require("./routes/auth"));
+
+// Manejar rutas
+app.length("*", (res, res) => {
+	res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
 	console.log(`Servidor corriendo en puerto ${process.env.PORT}`);
